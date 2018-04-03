71735a85d056bf0e94c8f6bcd938a6332d26a6e5
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-02 23:27:57 -0400

    added javascript, currently not working

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// The score number that will appear inside #score-number div
+var totalScoreNumber = 0;
+
+// Number of wins
+var wins = 0;
+
+// Number of losses
+var losses = 0;
+
+// When ruby image is clicked
+$(".thumbnail").on("click", "#ruby", function() {
+    //totalScoreNumber goes up by 1
+    totalScoreNumber += 1;
+    debugger;
+        //Adds new score to #total-score-number div
+        alert(totalScoreNumber);
+        console.log(totalScoreNumber);
+    }
+);
